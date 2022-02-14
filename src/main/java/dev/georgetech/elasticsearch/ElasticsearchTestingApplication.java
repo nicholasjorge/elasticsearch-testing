c4574ff0c421452cc8728858cc449c6cde8254cd
George Nicolae <nicolae.george.9@gmail.com>
2022-02-14 18:18:28 +0200

    First Test

---
+++ src/main/java/dev/georgetech/elasticsearch/ElasticsearchTestingApplication.java
@@ -0,0 +1,13 @@
+package dev.georgetech.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchTestingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ElasticsearchTestingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/georgetech/elasticsearch/ElasticsearchTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.georgetech.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchTestingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
